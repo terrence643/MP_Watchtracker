1d48a959bea4e82a9949a5e95d8743feaeb13149
terrence643 <68636531+terrence643@users.noreply.github.com>
2021-05-17 17:32:31 +0800

    Updated FragmenHome
    it allows for the getting of the link for popular movies

--- app/src/main/java/com/mobdeve/cait/mp/FragmentHome.java
+++ app/src/main/java/com/mobdeve/cait/mp/FragmentHome.java
@@ -1,5 +1,7 @@
 package com.mobdeve.cait.mp;
 
+
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,11 +10,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+
 /**
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
     public class FragmentHome extends Fragment {
+
+        private static String popularMovie = "https://api.themoviedb.org/3/movie/popular?api_key="+ BuildConfig.TMDB_API;
 
     public FragmentHome(){
         //Required empty public constructor
@@ -20,6 +26,7 @@ import android.view.ViewGroup;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
 
 
         // Inflate the layout for this fragment
