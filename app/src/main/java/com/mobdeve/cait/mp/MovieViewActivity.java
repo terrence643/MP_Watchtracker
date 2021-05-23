6246021a259f33f39e8bfce454056fd1b33675f7
deasisjhose <jhose_mhiguel_deasis@dlsu.edu.ph>
2021-05-24 03:05:14 +0800

    activity movie view

---
+++ app/src/main/java/com/mobdeve/cait/mp/MovieViewActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.cait.mp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_view);
+    }
+}
