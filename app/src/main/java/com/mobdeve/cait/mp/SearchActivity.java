9ea4f76f5033beabbb7df3568d68e4840e39024d
terrence643 <68636531+terrence643@users.noreply.github.com>
2021-05-31 17:18:01 +0800

    Revert "Update SearchActivity.java"
    This reverts commit cabbfa3aeb714967189cc91d8d2dd56cbf31e078.

--- app/src/main/java/com/mobdeve/cait/mp/SearchActivity.java
+++ app/src/main/java/com/mobdeve/cait/mp/SearchActivity.java
@@ -5,16 +5,9 @@ import android.os.Bundle;
 
 public class SearchActivity extends AppCompatActivity {
 
-    private static String searchMovie = "https://api.themoviedb.org/3/search/movie?api_key="+BuildConfig.TMDB_API+"&query="/*Insert edittext.gettext here*/;
-    private static String searchTv = "https://api.themoviedb.org/3/search/tv?api_key="+BuildConfig.TMDB_API+"&query=" /*Insert edittext.gettext here*/;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
-        
     }
 }
